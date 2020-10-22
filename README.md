4ce4f46721924e8c58cbf104d12865c8b6559f1f
rowich <49855876+rowich@users.noreply.github.com>
2020-10-22 15:03:08 +0200

    new optimised version
    - only one script
    - no temporary csv file
    - faster

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Teslalogger2gpx
  Extract GPS trip data from the Teslalogger database and convert to GPX file format
  
- v0.1 - very first initial release
+ v0.5 - speed and usage optimised release
+        only one script, no temporary CSV file, much faster
  
- code is based on Powershell (tested with Windows 10/2004 builtin Powershell v5.x [non Core])
+ Code is based on Powershell (tested with Windows 10/2004 built-in Powershell v5.x [non Core])
  
- use "Extract-Teslalogger-GPX.ps1" to extract GPS trip data from a Teslalogger backup database dump into a readable CSV (tab delimited) file. Writes the file ".\teslalogger.gpx.csv" into current working folder
- - Parameter "-Importfile" to specify the database backup dump file to read from
+ use "Teslalogger2gpx.ps1" to extract GPS trip data from a Teslalogger backup database dump and convert it to a GPX file
+ - Parameter "-Importfile" to specify the database backup dump file to read from. This is a mandatory parameter
  - Parameter "-Datestart" to specify the start date of the extract. Format is "yyyy-mm-dd", default is "2000-01-01"
  - Parameter "-Dateend" to specify the end date of the extract. Format is "yyyy-mm-dd", default is "9999-99-99"
- 
- use "Write-Teslalogger-GPX.ps1" to convert the CSV file into GPX file format. Reads ".\teslalogger.gpx.csv" and creates "teslalogger.gpx"
+ - Parameter "-ExportFile" to specify the output file in GPX format, default is "teslalogger.gpx" 
  
  no database interface so far.
  MariaDB SQL backup dump file must be extracted before it can be used. "Extract" script must be used with parameter "-Importfile" to specify the file
- 
-Separated into two steps to allow easier direct data retrieval from datasource, the second steps stays the same
+
+v0.01 - very first initial release
---
+++ Teslalogger2GPX.ps1
@@ -0,0 +1,122 @@
+﻿param([Parameter(Mandatory=$true)][string] $Importfile, [string] $DateStart = "2000-01-01", [string] $DateEnd = "9999-99-99", [string] $Exportfile = "teslalogger.gpx")
+
+if (-not (Test-Path $Importfile)) {
+  Write-Host "Import file $Importfile not found"
+  exit 1
+}
+
+$Starttime = Get-Date
+$GPX = @()
+
+
+#
+# extracts datasets from current datastream and creates the object using date, lat, lng and alt values
+# checks for datestart and dateend
+function process-line([string] $line) {
+
+    # remove trailing "insert into..." SQL command
+    $line = $line.Substring($line.IndexOf("(")+1)
+    $Param = @()
+    $Element = @()
+    $Splitdata = $line.Split("),(")
+    $Counter = 0
+    foreach($value in $Splitdata) {
+        $Counter++
+        if (($Counter % 100) -eq 0) {
+            Write-Progress -Activity "Deconstructing line of data" -Status "$param1" -PercentComplete ($Counter / $Splitdata.Count * 100) -ParentId 1 -Id 2
+        }
+
+        if ($value -eq "") {
+            if ($Param.Count -ge 10) {
+                $param1 = [string] $Param[1]
+                if ($param1.Substring(0,1) -eq "'") { $param1 = $param1.Substring(1,$param1.Length-2) }
+                if ($param1 -gt $DateEnd) { break }
+                if ($param1 -ge $DateStart) {
+                    $Element += [PSCustomObject] [ordered] @{ "Date" = $param1; "Lat" = $param[2]; "Lng" = $param[3]; "Alt" = $param[10] }
+                    if ($element.Count % 100 -eq 0) { write-host "." -NoNewline -ForegroundColor Gray }
+                }
+                $Param = @()
+            }
+        } else {
+            $Param += $Value
+        }
+    }
+    Write-Progress -Completed -Activity "line deconstruction completed" -ParentId 1 -Id 2
+
+    return $Element
+}
+
+#
+# main code starts here
+#
+
+# read data file and filter for SQL "insert into" commands
+Write-Host "Reading data from $Importfile into memory"
+$dataset = (Get-Content $Importfile -Encoding UTF8 | ? { $_ -like "INSERT INTO *"})
+Write-Host "Reading completed."
+$datacounter = 0
+foreach($line in  $dataset) {
+    Write-Progress -Activity "Processing data file $Importfile" -status "from $DateStart to $DateEnd" -PercentComplete (++$datacounter / $dataset.count * 100) -id 1
+
+    # we had issues to initially filter for "insert into `pos`" command. therefore do the second check here
+    if ($line.Substring(0,20) -like "*pos*") {
+        $GPX += process-line $line
+    }
+}
+Write-Progress -Activity "Completed" -Completed -id 1
+
+
+# write GPX file header
+Write-Output @"
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx version="1.1" creator="Teslalogger GPX COnverter" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3" elementFormDefault="qualified">
+<metadata>
+	<name>teslalogger.gpx</name>
+</metadata>
+"@ | Out-File -FilePath $Exportfile -Encoding utf8
+
+$Counter = 0
+$CntEntries = 0
+$DateLast = "n/a"
+$PosLast = "n/a"
+
+foreach($Element in $GPX) {
+    if ($Counter++ % 100 -eq 0) { Write-Progress -Activity "Writing GPX file $Exportfile" -Status $Element.Date -PercentComplete ($Counter / $GPX.Count * 100) -ParentId -1 }
+
+    # only create a new GPX record if the position has changed
+    $Pos = "lat=`"$($Element.lat)`" lon=`"$($Element.lng)`""
+    if ($Pos -ne $PosLast) {
+
+        # convert date/time into GPX format (insert a "T")
+        # do not convert time, GPX analysis tool must know the timezone. Default seems to be UTC. We don't know the originating time zone here, depends on the location of the car at time of recording
+        $Date = $Element.date.Substring(0,10) + "T" + $Element.Date.Substring(11)
+
+        # only create the elevation part if one exists
+        if ($Element.alt -ne "NULL") {
+            $alt = "<ele>$($Element.alt)</ele>"
+        } else {
+            $alt = ""
+        }
+
+        # create new Track element if day has changed since last element. New track node gets the name of the day (allows filtering for days later on)
+        if ($DateLast -ne $Element.Date.Substring(0,10)) {
+            if ($DateLast -ne "n/a") { Write-Output "</trkseg></trk>" | Out-File -FilePath $Exportfile -Append -Encoding utf8 }
+            $DateLast = $Element.Date.Substring(0,10)
+            Write-Output "<trk><name>$DateLast</name><trkseg>" | Out-File -FilePath $Exportfile -Append -Encoding utf8
+        }
+
+        Write-Output "    <trkpt $Pos>$alt<time>$date</time></trkpt>" | out-file -FilePath $Exportfile -Append -Encoding utf8
+        $PosLast = $Pos
+        $CntEntries++
+    }
+}
+
+# write GPX file footer
+Write-Output @"
+	</trkseg>
+</trk>
+</gpx>
+"@ | Out-File -FilePath $Exportfile -Append -Encoding utf8
+Write-Progress -Completed -Activity "Done"
+$Time = (Get-date - $Starttime) -f "HH:mm:ss"
+Write-Host "`r`nCompleted. $CntEntries GPX elemented written, $Time required"
